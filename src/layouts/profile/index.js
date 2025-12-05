403a33ef96b3a5ff971701f5a9d68a521bda89b3
Hassan <hm350074@gmail.com>
2025-12-05 20:17:46 +0500

    first commit

---
+++ src/layouts/profile/index.js
@@ -0,0 +1,130 @@
+/*!
+
+=========================================================
+* Vision UI Free React - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vision-ui-free-react
+* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
+* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-react/blob/master LICENSE.md)
+
+* Design and Coded by Simmmple & Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+// @mui material components
+// @mui icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
+
+import Grid from "@mui/material/Grid";
+
+// Vision UI Dashboard React components
+import VuiBox from "components/VuiBox";
+
+import ProfileInfoCard from "examples/Cards/InfoCards/ProfileInfoCard";
+
+import Footer from "examples/Footer";
+// Vision UI Dashboard React example components
+import DashboardLayout from "examples/LayoutContainers/DashboardLayout";
+// Overview page components
+import Header from "layouts/profile/components/Header";
+
+import Welcome from "../profile/components/Welcome/index";
+import CarInformations from "./components/CarInformations";
+
+function Overview() {
+  return (
+    <DashboardLayout>
+      <Header />
+      <VuiBox mt={5} mb={3}>
+        <Grid
+          container
+          spacing={3}
+          sx={({ breakpoints }) => ({
+            [breakpoints.only("xl")]: {
+              gridTemplateColumns: "repeat(2, 1fr)",
+            },
+          })}
+        >
+          <Grid
+            item
+            xs={12}
+            xl={4}
+            xxl={3}
+            sx={({ breakpoints }) => ({
+              minHeight: "400px",
+              [breakpoints.only("xl")]: {
+                gridArea: "1 / 1 / 2 / 2",
+              },
+            })}
+          >
+            <Welcome />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            xl={5}
+            xxl={6}
+            sx={({ breakpoints }) => ({
+              [breakpoints.only("xl")]: {
+                gridArea: "2 / 1 / 3 / 3",
+              },
+            })}
+          >
+            <CarInformations />
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            xl={3}
+            xxl={3}
+            sx={({ breakpoints }) => ({
+              [breakpoints.only("xl")]: {
+                gridArea: "1 / 2 / 2 / 3",
+              },
+            })}
+          >
+            <ProfileInfoCard
+              title="profile information"
+              description="Hi, I’m Mark Johnson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality)."
+              info={{
+                fullName: "Mark Johnson",
+                mobile: "(44) 123 1234 123",
+                email: "mark@simmmple.com",
+                location: "United States",
+              }}
+              social={[
+                {
+                  link: "https://www.facebook.com/CreativeTim/",
+                  icon: <FacebookIcon />,
+                  color: "facebook",
+                },
+                {
+                  link: "https://twitter.com/creativetim",
+                  icon: <TwitterIcon />,
+                  color: "twitter",
+                },
+                {
+                  link: "https://www.instagram.com/creativetimofficial/",
+                  icon: <InstagramIcon />,
+                  color: "instagram",
+                },
+              ]}
+            />
+          </Grid>
+        </Grid>
+      </VuiBox>
+   
+
+      <Footer />
+    </DashboardLayout>
+  );
+}
+
+export default Overview;
---
+++ src/routes.js
@@ -0,0 +1,52 @@
+
+
+// Vision UI Dashboard React layouts
+import Dashboard from "layouts/dashboard";
+import Tables from "layouts/tables";
+
+
+import Profile from "layouts/profile";
+
+
+
+
+import { BsFillPersonFill } from "react-icons/bs";
+
+
+import { IoStatsChart } from "react-icons/io5";
+import { IoHome } from "react-icons/io5";
+
+const routes = [
+  {
+    type: "collapse",
+    name: "Dashboard",
+    key: "dashboard",
+    route: "/dashboard",
+    icon: <IoHome size="15px" color="inherit" />,
+    component: Dashboard,
+    noCollapse: true,
+  },
+  {
+    type: "collapse",
+    name: "Tables",
+    key: "tables",
+    route: "/tables",
+    icon: <IoStatsChart size="15px" color="inherit" />,
+    component: Tables,
+    noCollapse: true,
+  },
+
+  { type: "title", title: "Account Pages", key: "account-pages" },
+  {
+    type: "collapse",
+    name: "Profile",
+    key: "profile",
+    route: "/profile",
+    icon: <BsFillPersonFill size="15px" color="inherit" />,
+    component: Profile,
+    noCollapse: true,
+  },
+
+];
+
+export default routes;
